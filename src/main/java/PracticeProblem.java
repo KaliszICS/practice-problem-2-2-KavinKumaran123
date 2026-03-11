92342b64042626e0599526574ba52f21ef5eed53
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-03-11 23:43:46 +0000

    2.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,73 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-		
-	}
-	
+public static int[] compareSearch(int[] array, int number) {
+    int sequentialLoops = 0;
+    int binaryLoops = 0;
+
+
+    for (int i = 0; i < array.length; i++) {
+        sequentialLoops++;
+        if (array[i] == number) {
+            break;
+        }
+    }
+
+    int low = 0;
+    int high = array.length - 1;
+
+    while (low <= high) {
+        binaryLoops++;
+        int mid = (low + high) / 2;
+
+        if (array[mid] == number) {
+            break;
+        } else if (array[mid] < number) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return new int[]{sequentialLoops, binaryLoops};
+}
+	public static int[] compareStringSearch(String[] array, String word) {
+
+    int sequentialLoops = 0;
+    int binaryLoops = 0;
+
+
+   		for (int i = 0; i < array.length; i++) {
+        	sequentialLoops++;
+        	if (array[i].equals(word)) {
+            	break;
+        	}
+    	}
+
+    	int left = 0;
+    	int right = array.length - 1;
+
+    	while (left <= right) {
+        	binaryLoops++;
+
+        	int mid = (left + right) / 2;
+
+        	int compare = array[mid].compareTo(word);
+
+        	if (compare == 0) {
+            	break;
+        	}
+        	else if (compare < 0) {
+            	left = mid + 1;
+        	}
+        	else {
+            	right = mid - 1;
+        	}
+    	}
+
+    	int[] result = new int[2];
+    	result[0] = sequentialLoops;
+    	result[1] = binaryLoops;
+
+    	return result;
+	} 
 }
